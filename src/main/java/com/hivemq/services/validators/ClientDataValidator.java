8938884c263097a651cd010509ce5e5f11b6fdb2
Ivan Usenka <vanek66650@gmail.com>
2017-08-04 10:18:44 +0300

    Client data validation refactoring

--- src/main/java/com/hivemq/services/validators/ClientDataValidator.java
+++ src/main/java/com/hivemq/services/validators/ClientDataValidator.java
@@ -3,23 +3,20 @@ package com.hivemq.services.validators;
 import com.google.common.base.Optional;
 import com.google.common.base.Strings;
 import com.hivemq.domain.HiveMQClientsData;
-import com.hivemq.spi.aop.cache.Cached;
 import com.hivemq.spi.security.ClientCredentialsData;
 import com.hivemq.spi.security.ClientData;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Ivan Usenka on 01-Aug-17.
  */
 public class ClientDataValidator {
 
-    //TODO refactor this to avoid multiple file loading when DB microservice is ready
-
     private final ClientData clientData;
+    private final HiveMQClientsData mqClientsData;
 
     public ClientDataValidator(ClientData clientData, HiveMQClientsData mqClientsData) {
         this.clientData = clientData;
+        this.mqClientsData = mqClientsData;
     }
 
     public boolean isIpInRange() {
